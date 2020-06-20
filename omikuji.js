e820f4725260cae7d7dfcaf9bc8dd17d4ed71f2b
kiichifukuzawa <back19future85f@outlook.jp>
2020-06-20 09:23:04 +0900

    名前が未入力のとき名無しと表示する

--- omikuji.js
+++ omikuji.js
@@ -2,9 +2,12 @@ var username;
 var userresult;
  
 username = prompt("お名前を教えて下さい。");
+if (username == ""){
+	username = "名無し"
+}
 document.getElementById("name").innerHTML = username;
  var rand = Math.floor( Math.random() * 5) ; 
- 
+
  if (rand == 0) {
 	userresult = "大吉";
 }
